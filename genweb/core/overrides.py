072aadc8f26312230831b4f0458084381186b804
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-03-21 12:43:07 +0100

    New overrides of menus, languages settings, imaging image settings

--- genweb/core/overrides.py
+++ genweb/core/overrides.py
@@ -1,7 +1,12 @@
-from plone.app.contentmenu.menu import DisplaySubMenuItem
+from plone.app.contentmenu.menu import DisplaySubMenuItem, FactoriesSubMenuItem
 from genweb.core import GenwebMessageFactory as _
 
 
 class gwDisplaySubMenuItem(DisplaySubMenuItem):
 
     title = _(u'label_choose_template', default=u'Display')
+
+
+class gwFactoriesSubMenuItem(FactoriesSubMenuItem):
+
+    title = _(u'label_add_new_item', default=u'Add new\u2026')
