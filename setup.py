c44a4b386c0a5ca9b1e4fe9fb486bed9a10ef1eb
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-05-26 15:22:14 +0200

    Preparing release 4.3.18

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.3.18.dev0'
+version = '4.3.18'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
