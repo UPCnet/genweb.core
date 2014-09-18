108dbdf0c561dc19c84ddd6506166a52fd694608
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-09-18 17:37:22 +0200

    Search patch and i18n

--- genweb/core/patches.py
+++ genweb/core/patches.py
@@ -1,6 +1,11 @@
+from plone.app.search.browser import quote_chars
+from plone.app.search.browser import EVER
+
 from Products.PlonePAS.utils import safe_unicode
 from Products.LDAPUserFolder.utils import guid2string
 from Products.LDAPUserFolder.LDAPDelegate import filter_format
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.browser.navtree import getNavigationRoot
 
 import unicodedata
 
@@ -179,3 +184,47 @@ def generate_user_id(self, data):
     default = data.get('username').lower() or data.get('email').lower() or ''
     data['username'] = default
     return default
+
+
+def filter_query(self, query):
+    request = self.request
+
+    catalog = getToolByName(self.context, 'portal_catalog')
+    valid_indexes = tuple(catalog.indexes())
+    valid_keys = self.valid_keys + valid_indexes
+
+    text = query.get('SearchableText', None)
+    if text is None:
+        text = request.form.get('SearchableText', '')
+    if not text:
+        # Without text, must provide a meaningful non-empty search
+        valid = set(valid_indexes).intersection(request.form.keys()) or \
+            set(valid_indexes).intersection(query.keys())
+        if not valid:
+            return
+
+    for k, v in request.form.items():
+        if v and ((k in valid_keys) or k.startswith('facet.')):
+            query[k] = v
+    if text:
+        query['SearchableText'] = quote_chars(text) + '*'
+
+    # don't filter on created at all if we want all results
+    created = query.get('created')
+    if created:
+        if created.get('query'):
+            if created['query'][0] <= EVER:
+                del query['created']
+
+    # respect `types_not_searched` setting
+    types = query.get('portal_type', [])
+    if 'query' in types:
+        types = types['query']
+    query['portal_type'] = self.filter_types(types)
+    # respect effective/expiration date
+    query['show_inactive'] = False
+    # respect navigation root
+    if 'path' not in query:
+        query['path'] = getNavigationRoot(self.context)
+
+    return query
