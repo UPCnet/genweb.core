95d0bc73c352dd65f10a0b7b8a904150e6534ffc
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-09-23 13:27:28 +0200

    Patch for fixing the wcfc error on deleting objects.

--- genweb/core/patches.py
+++ genweb/core/patches.py
@@ -228,3 +228,59 @@ def filter_query(self, query):
         query['path'] = getNavigationRoot(self.context)
 
     return query
+
+
+# TOREMOVE as soon 4.3.4 goes live.
+import transaction
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from ZODB.POSException import ConflictError
+from plone.app.linkintegrity.exceptions import LinkIntegrityNotificationException
+from Products.CMFPlone.utils import log_exc
+from Products.CMFPlone.utils import transaction_note
+
+
+def deleteObjectsByPaths(self, paths, handle_errors=True, REQUEST=None):
+    failure = {}
+    success = []
+    # use the portal for traversal in case we have relative paths
+    portal = getToolByName(self, 'portal_url').getPortalObject()
+    traverse = portal.restrictedTraverse
+    for path in paths:
+        # Skip and note any errors
+        if handle_errors:
+            sp = transaction.savepoint(optimistic=True)
+        try:
+            # To avoid issues with the check for acquisition,
+            # relative paths should not be part of the API anymore.
+            # Plone core itself does not use relative paths.
+            if not path.startswith("/".join(portal.getPhysicalPath())):
+                msg = (
+                    'Path {} does not start '
+                    'with path to portal'.format(path)
+                )
+                raise ValueError(msg)
+            obj = traverse(path)
+            if list(obj.getPhysicalPath()) != path.split('/'):
+                msg = (
+                    'Path {} does not match '
+                    'traversed object physical path. '
+                    'This is likely an acquisition issue.'.format(path)
+                )
+                raise ValueError(msg)
+            obj_parent = aq_parent(aq_inner(obj))
+            obj_parent.manage_delObjects([obj.getId()])
+            success.append('%s (%s)' % (obj.getId(), path))
+        except ConflictError:
+            raise
+        except LinkIntegrityNotificationException:
+            raise
+        except Exception, e:
+            if handle_errors:
+                sp.rollback()
+                failure[path] = e
+                log_exc()
+            else:
+                raise
+    transaction_note('Deleted %s' % (', '.join(success)))
+    return success, failure
