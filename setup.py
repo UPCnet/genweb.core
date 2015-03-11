528267c20a59bf763a30b516e381dbfbff89b1f2
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-03-11 11:11:33 +0100

    Back to development: 4.5.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.5.0'
+version = '4.5.1.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
