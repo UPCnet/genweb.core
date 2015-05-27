5e392c03d6b738819867c64e9f4cf8051cbcb79a
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-05-27 13:03:09 +0200

    Disable right column in DX add forms

--- genweb/core/browser/dxforms.py
+++ genweb/core/browser/dxforms.py
@@ -1,4 +1,7 @@
+from zope.event import notify
+from plone.dexterity.events import AddBegunEvent
 from plone.dexterity.browser import edit
+from plone.dexterity.browser.add import DefaultAddForm
 
 
 class EditForm(edit.DefaultEditForm):
@@ -8,4 +11,16 @@ class EditForm(edit.DefaultEditForm):
 
     def update(self):
         super(EditForm, self).update()
+
+        # Patched for all dexterity types, disable right column
         self.request.set('disable_plone.rightcolumn', True)
+
+
+def update(self):
+    super(DefaultAddForm, self).update()
+    # fire the edit begun only if no action was executed
+    if len(self.actions.executedActions) == 0:
+        notify(AddBegunEvent(self.context))
+
+    # Patched for all dexterity types, disable right column
+    self.request.set('disable_plone.rightcolumn', True)
