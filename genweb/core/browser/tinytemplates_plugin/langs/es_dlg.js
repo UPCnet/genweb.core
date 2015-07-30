07a94a47cce04e59dc5cfb56a0762ded67e46e0d
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-07-30 13:24:43 +0200

    Fix templates

---
+++ genweb/core/browser/tinytemplates_plugin/langs/es_dlg.js
@@ -0,0 +1,15 @@
+tinyMCE.addI18n('es.template_dlg',{
+title:"Plantillas",
+label:"Plantilla",
+desc_label:"Descripción",
+desc:"Inserta contenido predefinido en la página",
+select:"Selecciona una plantilla",
+preview:"Previsualización",
+warning:"Atención: Actualizar una plantilla con el contenido de otra puede causar pérdida de datos.",
+mdate_format:"%d-%m-%Y %H:%M:%S",
+cdate_format:"%d-%m-%Y %H:%M:%S",
+months_long:"January,February,March,April,May,June,July,August,September,October,November,December",
+months_short:"Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec",
+day_long:"Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday",
+day_short:"Sun,Mon,Tue,Wed,Thu,Fri,Sat,Sun"
+});
