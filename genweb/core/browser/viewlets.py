13c66fb105242390885c430b515326c9e00e944a
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-09-07 14:29:54 +0200

    New resource viewlet base class

--- genweb/core/browser/viewlets.py
+++ genweb/core/browser/viewlets.py
@@ -1,19 +1,21 @@
 from five import grok
+from plone import api
+from zope.interface import Interface
 from urllib import quote
 from ZTUtils import make_query
 
-from Products.CMFPlone.interfaces import IPloneSiteRoot
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
+from plone.memoize import forever
 from plone.uuid.interfaces import IUUID
 from plone.app.i18n.locales.browser.selector import LanguageSelector
-from zope.interface import Interface
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from genweb.core.utils import genweb_config, havePermissionAtRoot
 from genweb.core.interfaces import IGenwebLayer
 from genweb.core import GenwebMessageFactory as _
 
+import json
 import pkg_resources
 
 try:
@@ -24,6 +26,81 @@ except pkg_resources.DistributionNotFound:
 else:
     HAS_LINGUAPLONE = True
     from Products.LinguaPlone.interfaces import ITranslatable
+
+
+class gwCSSViewletManager(grok.ViewletManager):
+    grok.context(Interface)
+    grok.name('genweb.css')
+
+
+class gwJSViewletManager(grok.ViewletManager):
+    grok.context(Interface)
+    grok.name('genweb.js')
+
+
+class baseResourcesViewlet(grok.Viewlet):
+    """ This is the base CSS and JS viewlet. """
+    grok.baseclass()
+
+    resource_type = None
+    current_egg_name = None
+
+    def is_devel_mode(self):
+        return api.env.debug_mode()
+
+    def read_resource_config_file(self):
+        egg = pkg_resources.get_distribution(self.current_egg_name)
+        config_path = '{}/{}/config.json'.format(egg.location, self.current_egg_name.replace('.', '/'))
+        resource_file = open(config_path)
+        return resource_file.read()
+
+    def get_resources(self):
+        if self.is_devel_mode:
+            return self.get_development_resources()
+        else:
+            return self.get_production_resources()
+
+    @forever.memoize
+    def get_development_resources(self):
+        true_http_path = []
+        resources_conf = json.loads(self.read_resource_config_file())
+        replace_map = resources_conf['replace_map']
+
+        for kind in resources_conf['order']:
+            devel_resources = resources_conf['resources'][kind][self.resource_type]['development']
+            for resource in devel_resources:
+                found = False
+                for source, destination in replace_map.items():
+                    if source in resource:
+                        true_http_path.append(resource.replace(source, destination))
+                        found = True
+                if not found:
+                    true_http_path.append(resource)
+
+        return true_http_path
+
+    @forever.memoize
+    def get_production_resources(self):
+        true_http_path = []
+        resources_conf = json.loads(self.read_resource_config_file())
+        replace_map = resources_conf['replace_map']
+        for kind in resources_conf['order']:
+            production_resources = resources_conf['resources'][kind][self.resource_type]['production']
+            for resource in production_resources:
+                for res_rev_key in resources_conf['revision_info']:
+                    if resource == res_rev_key:
+                        resource = resources_conf['revision_info'][res_rev_key]
+
+                found = False
+                for source, destination in replace_map.items():
+                    if source in resource:
+                        true_http_path.append(resource.replace(source, destination))
+                        found = True
+                if not found:
+                    true_http_path.append(resource)
+
+        return true_http_path
+
 
 # [DEPRECATED] All this viewlets and associated code are deprecated in favor of
 # the PAM aware viewlet genweb.pamls
@@ -222,13 +299,3 @@ class gwLanguageSelectorForRoot(gwLanguageSelectorBase):
             results.append(data)
 
         return results
-
-
-class gwCSSViewletManager(grok.ViewletManager):
-    grok.context(Interface)
-    grok.name('genweb.css')
-
-
-class gwJSViewletManager(grok.ViewletManager):
-    grok.context(Interface)
-    grok.name('genweb.js')
