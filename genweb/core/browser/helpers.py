14e00438d54df9f117e4c1a7baf7fbec66e7a097
roberto.diaz <roberto.diaz@upcnet.es>
2016-06-02 18:08:16 +0200

    retrieve white_list from Tiny again...

--- genweb/core/browser/helpers.py
+++ genweb/core/browser/helpers.py
@@ -1463,28 +1463,52 @@ class ModifyWhiteListSafeHTML(grok.View):
     def render(self, portal=None):
         if CSRF:
             alsoProvides(self.request, IDisableCSRFProtection)
-        portal = api.portal.get()
-        transforms = getToolByName(portal, 'portal_transforms')
-        transform = getattr(transforms, 'safe_html')
-        whitelist = transform.get_parameter_value('style_whitelist')
-        if 'text-decoration' not in whitelist:
-            whitelist.append(u'text-decoration')
-        if 'height' not in whitelist:
-            whitelist.append(u'height')
-        if 'width' not in whitelist:
-            whitelist.append(u'width')
 
-        kwargs = {}
-        kwargs['style_whitelist'] = whitelist
-        for k in list(kwargs):
-            if isinstance(kwargs[k], dict):
-                v = kwargs[k]
-                kwargs[k + '_key'] = v.keys()
-                kwargs[k + '_value'] = [str(s) for s in v.values()]
-                del kwargs[k]
-        transform.set_parameters(**kwargs)
-        transform._p_changed = True
-        transform.reload()
+        from plone import api
+        from Products.PortalTransforms.Transform import make_config_persistent
+
+        tid = 'safe_html'
+
+        pt = api.portal.get_tool(name='portal_transforms')
+        if not tid in pt.objectIds(): return
+
+        trans = pt[tid]
+
+        tconfig = trans._config
+        tconfig['class_blacklist'] = []
+        tconfig['nasty_tags'] = {'meta': '1'}
+        tconfig['remove_javascript'] = 0
+        tconfig['stripped_attributes'] = ['lang', 'valign', 'halign', 'border',
+                                          'frame', 'rules', 'cellspacing',
+                                          'cellpadding', 'bgcolor']
+        tconfig['stripped_combinations'] = {}
+        tconfig['style_whitelist'] = ['text-align', 'list-style-type', 'float',
+                                      'width', 'height', 'padding-left',
+                                      'padding-right', 'text-decoration']
+
+        tconfig['valid_tags'] = {
+         'code': '1', 'meter': '1', 'tbody': '1', 'style': '1', 'img': '0',
+         'title': '1', 'tt': '1', 'tr': '1', 'param': '1', 'li': '1',
+         'source': '1', 'tfoot': '1', 'th': '1', 'td': '1', 'dl': '1',
+         'blockquote': '1', 'big': '1', 'dd': '1', 'kbd': '1', 'dt': '1',
+         'p': '1', 'small': '1', 'output': '1', 'div': '1', 'em': '1',
+         'datalist': '1', 'hgroup': '1', 'video': '1', 'rt': '1', 'canvas': '1',
+         'rp': '1', 'sub': '1', 'bdo': '1', 'sup': '1', 'progress': '1',
+         'body': '1', 'acronym': '1', 'base': '0', 'br': '0', 'address': '1',
+         'article': '1', 'strong': '1', 'ol': '1', 'script': '1', 'caption': '1',
+         'dialog': '1', 'col': '1', 'h2': '1', 'h3': '1', 'h1': '1', 'h6': '1',
+         'h4': '1', 'h5': '1', 'header': '1', 'table': '1', 'span': '1',
+         'area': '0', 'mark': '1', 'dfn': '1', 'var': '1', 'cite': '1',
+         'thead': '1', 'head': '1', 'hr': '0', 'link': '1', 'ruby': '1',
+         'b': '1', 'colgroup': '1', 'keygen': '1', 'ul': '1', 'del': '1',
+         'iframe': '1', 'embed': '1', 'pre': '1', 'figure': '1', 'ins': '1',
+         'aside': '1', 'html': '1', 'nav': '1', 'details': '1', 'u': '1',
+         'samp': '1', 'map': '1', 'object': '1', 'a': '1', 'footer': '1',
+         'i': '1', 'q': '1', 'command': '1', 'time': '1', 'audio': '1',
+         'section': '1', 'abbr': '1'}
+        make_config_persistent(tconfig)
+        trans._p_changed = True
+        trans.reload()
 
 
 class ListDomaninsCache(grok.View):
