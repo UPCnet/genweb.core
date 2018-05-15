b2cec99966a96039eba76f1282a17c80e52bd803
oscar.albareda <oscar.albareda@upc.edu>
2018-05-15 09:11:44 +0200

    Prova

--- genweb/core/browser/tinytemplates_plugin/js/template.js
+++ genweb/core/browser/tinytemplates_plugin/js/template.js
@@ -2,55 +2,35 @@ tinyMCEPopup.requireLangPack();
 
 var TemplateDialog = {
     preInit : function() {
-        
-        // Use this parameter to load another JavaScript file with template
-        // definitions. By default, we load our own list using the semantics
-        // described in the README.
-        var url = tinyMCEPopup.getParam("template_external_list_url");
-
-        if (url != null)
-            document.write('<sc'+'ript language="javascript" type="text/javascript" src="' + tinyMCEPopup.editor.documentBaseURI.toAbsolute(url) + '"></sc'+'ript>');
+       
     },
 
     init : function() {
-        var ed = tinyMCEPopup.editor, tsrc, sel, x, u;
+        var editor = tinyMCEPopup.editor;
+        var popup = document.forms[0];
+        var popupcontent = window.frames['templatesrc'].document;
+
+        var body = editor.getBody();
+        var items = body.getElementsByClassName('item');
+        console.log(items);
         
-        
-        // Load templates from an explicit parameter. By default, we don't
-        // use this
-        tsrc = ed.getParam("template_templates", false);
-        sel = document.getElementById('tpath');
+        // desplegable
+        var sel = document.getElementById('tpath');
 
-        // Use external template list as a fallback
-        if (!tsrc && typeof(tinyMCETemplateList) != 'undefined') {
-            for (x=0, tsrc = []; x<tinyMCETemplateList.length; x++)
-                tsrc.push({title : tinyMCETemplateList[x][0], src : tinyMCETemplateList[x][1], description : tinyMCETemplateList[x][2]});
-        }
+        // busquem el carousel selecionat en l'editor
+        e = editor.selection.getNode();
+        carousel = tinyMCE.DOM.getParent(e, function(p){ if (tinyMCE.DOM.hasClass(p,'carousel')) return p; }, body);
+        items = tinyMCE.DOM.select('.item',carousel);
 
-        for (x=0; x<tsrc.length; x++)
-            sel.options[sel.options.length] = new Option(tsrc[x].title, tinyMCEPopup.editor.documentBaseURI.toAbsolute(tsrc[x].src));
-
-        this.resize();
-        this.tsrc = tsrc;
-    },
-
-    resize : function() {
-        var w, h, e;
-
-        if (!self.innerWidth) {
-            w = document.body.clientWidth - 50;
-            h = document.body.clientHeight - 160;
-        } else {
-            w = self.innerWidth - 50;
-            h = self.innerHeight - 170;
+        for (var x=1; x<items.length+1; x++) {
+            sel.options[sel.options.length] = new Option('slide ' + x);
         }
         
-        e = document.getElementById('templatesrc');
+        popupcontent.body.innerHTML = items[0].outerHTML;
+        //document.getElementById('mceTemplatePreview').innerHTML = items[0].outerHTML;
 
-        if (e) {
-            e.style.height = Math.abs(h - 80) + 'px';
-            e.style.width  = Math.abs(w - 100) + 'px';
-        }
+
+        this.templateHTML = carousel.outerHTML;
     },
 
     loadCSSFiles : function(d) {
@@ -84,29 +64,6 @@ var TemplateDialog = {
         tinyMCEPopup.close();
     },
 
-    getFileContents : function(u) {
-        var x, d, t = 'text/plain';
-
-        function g(s) {
-            x = 0;
-
-            try {
-                x = new ActiveXObject(s);
-            } catch (s) {
-            }
-
-            return x;
-        };
-
-        x = window.ActiveXObject ? g('Msxml2.XMLHTTP') || g('Microsoft.XMLHTTP') : new XMLHttpRequest();
-
-        // Synchronous AJAX load file
-        x.overrideMimeType && x.overrideMimeType(t);
-        x.open("GET", u, false);
-        x.send(null);
-
-        return x.responseText;
-    }
 };
 
 TemplateDialog.preInit();
