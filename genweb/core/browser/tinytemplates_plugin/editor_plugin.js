052d5ce0a0fef48d5634cbfd37d51cf9a228e920
oscar.albareda <oscar.albareda@upc.edu>
2018-05-17 13:59:14 +0200

    Neteja codi sobrant

--- genweb/core/browser/tinytemplates_plugin/editor_plugin.js
+++ genweb/core/browser/tinytemplates_plugin/editor_plugin.js
@@ -27,71 +27,16 @@
                 });
             });
 
+            // Executa acció del popup
             ed.addCommand('mcePloneInsertTemplate', t._insertTemplate, t);
 
-            // Register buttons
-            ed.addButton('row_props', {title : 'table.row_desc', cmd : 'mceTableRowProps'});
-            ed.addButton('cell_props', {title : 'table.cell_desc', cmd : 'mceTableCellProps'});
+            // Register buttons: afegeix el botó a la botonera del tiny
             ed.addButton('plonetemplates', {title : 'template.desc', cmd : 'mcePloneTemplates'});
-
-            ed.onPreProcess.add(function(ed, o) {
-                var dom = ed.dom;
-
-                each(dom.select('div', o.node), function(e) {
-                    if (dom.hasClass(e, 'mceTmpl')) {
-                        each(dom.select('*', e), function(e) {
-                            if (dom.hasClass(e, ed.getParam('template_mdate_classes', 'mdate').replace(/\s+/g, '|')))
-                                e.innerHTML = t._getDateTime(new Date(), ed.getParam("template_mdate_format", ed.getLang("template.mdate_format")));
-                        });
-
-                        t._replaceVals(e);
-                    }
-                });
-            });
-
-            // Handle node change updates
-            ed.onNodeChange.add(function(ed, cm, n) {
-                var p;
-
-                n = ed.selection.getStart();
-                p = ed.dom.getParent(n, 'td,th,caption');
-                cm.setActive('table', n.nodeName === 'TABLE' || !!p);
-
-                // Disable table tools if we are in caption
-                if (p && p.nodeName === 'CAPTION')
-                    p = 0;
-
-                cm.setDisabled('delete_table', !p);
-                cm.setDisabled('delete_col', !p);
-                cm.setDisabled('delete_table', !p);
-                cm.setDisabled('delete_row', !p);
-                cm.setDisabled('col_after', !p);
-                cm.setDisabled('col_before', !p);
-                cm.setDisabled('row_after', !p);
-                cm.setDisabled('row_before', !p);
-                cm.setDisabled('row_props', !p);
-                cm.setDisabled('cell_props', !p);
-                cm.setDisabled('split_cells', !p);
-                cm.setDisabled('merge_cells', !p);
-            });
-        },
-
-        getInfo : function() {
-            return {
-                longname : 'Template plugin',
-                author : 'Moxiecode Systems AB',
-                authorurl : 'http://www.moxiecode.com',
-                infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/template',
-                version : tinymce.majorVersion + "." + tinymce.minorVersion
-            };
         },
 
         _insertTemplate : function(ui, v) {
-            var t = this;
-            var ed = t.editor; 
+            var ed = this.editor; 
             var h, el; 
-            var dom = ed.dom;  
-            var sel = ed.selection.getContent();
 
             // Elimina els items del carousel
             var items = v['carousel'].getElementsByClassName('item');
@@ -101,96 +46,14 @@
             }
 
             // i substitueix pel nou
+            v['carousel'].innerHTML = v.content;
 
-            v['carousel'].innerHTML=v.content;
-
-            each(t.editor.getParam('template_replace_values'), function(v, k) {
-                if (typeof(v) != 'function')
-                    h = h.replace(new RegExp('\\{\\$' + k + '\\}', 'g'), v);
-            });
-
-            el = dom.create('div', null, h);
-
-            // Find template element within div
-            n = dom.select('.mceTmpl', el);
-            if (n && n.length > 0) {
-                el = dom.create('div', null);
-                el.appendChild(n[0].cloneNode(true));
-            }
-
-            function hasClass(n, c) {
-                return new RegExp('\\b' + c + '\\b', 'g').test(n.className);
-            };
-
-            each(dom.select('*', el), function(n) {
-                // Replace cdate
-                if (hasClass(n, ed.getParam('template_cdate_classes', 'cdate').replace(/\s+/g, '|')))
-                    n.innerHTML = t._getDateTime(new Date(), ed.getParam("template_cdate_format", ed.getLang("template.cdate_format")));
-
-                // Replace mdate
-                if (hasClass(n, ed.getParam('template_mdate_classes', 'mdate').replace(/\s+/g, '|')))
-                    n.innerHTML = t._getDateTime(new Date(), ed.getParam("template_mdate_format", ed.getLang("template.mdate_format")));
-
-                // Replace selection
-                if (hasClass(n, ed.getParam('template_selected_content_classes', 'selcontent').replace(/\s+/g, '|')))
-                    n.innerHTML = sel;
-            });
-
-            t._replaceVals(el);
+            el = ed.dom.create('div', null, h);
 
             ed.execCommand('mceInsertContent', false, el.innerHTML);
             ed.addVisual();
         },
 
-        _replaceVals : function(e) {
-            var dom = this.editor.dom, vl = this.editor.getParam('template_replace_values');
-
-            each(dom.select('*', e), function(e) {
-                each(vl, function(v, k) {
-                    if (dom.hasClass(e, k)) {
-                        if (typeof(vl[k]) == 'function')
-                            vl[k](e);
-                    }
-                });
-            });
-        },
-
-        _getDateTime : function(d, fmt) {
-                if (!fmt)
-                    return "";
-
-                function addZeros(value, len) {
-                    var i;
-
-                    value = "" + value;
-
-                    if (value.length < len) {
-                        for (i=0; i<(len-value.length); i++)
-                            value = "0" + value;
-                    }
-
-                    return value;
-                }
-
-                fmt = fmt.replace("%D", "%m/%d/%y");
-                fmt = fmt.replace("%r", "%I:%M:%S %p");
-                fmt = fmt.replace("%Y", "" + d.getFullYear());
-                fmt = fmt.replace("%y", "" + d.getYear());
-                fmt = fmt.replace("%m", addZeros(d.getMonth()+1, 2));
-                fmt = fmt.replace("%d", addZeros(d.getDate(), 2));
-                fmt = fmt.replace("%H", "" + addZeros(d.getHours(), 2));
-                fmt = fmt.replace("%M", "" + addZeros(d.getMinutes(), 2));
-                fmt = fmt.replace("%S", "" + addZeros(d.getSeconds(), 2));
-                fmt = fmt.replace("%I", "" + ((d.getHours() + 11) % 12 + 1));
-                fmt = fmt.replace("%p", "" + (d.getHours() < 12 ? "AM" : "PM"));
-                fmt = fmt.replace("%B", "" + this.editor.getLang("template_months_long").split(',')[d.getMonth()]);
-                fmt = fmt.replace("%b", "" + this.editor.getLang("template_months_short").split(',')[d.getMonth()]);
-                fmt = fmt.replace("%A", "" + this.editor.getLang("template_day_long").split(',')[d.getDay()]);
-                fmt = fmt.replace("%a", "" + this.editor.getLang("template_day_short").split(',')[d.getDay()]);
-                fmt = fmt.replace("%%", "%");
-
-                return fmt;
-        }
     });
 
     // Register plugin
