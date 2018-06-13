8ba2c8503b81ecfcdf1c4505d5d08765e59853d2
alberto.duran <alberto.duran@upcnet.es>
2018-06-13 12:41:07 +0200

    Vista para exportar contenidos dexterity en json

--- genweb/core/browser/helpers_getters.py
+++ genweb/core/browser/helpers_getters.py
@@ -469,3 +469,26 @@ class getCollectionDefaultPages(grok.View):
             **dict(body='\n'.join(
                 [getCollectionDefaultPages.REPORT_ROW.format(**default_page)
                  for default_page in collections])))
+
+try:
+    from collective.jsonify.export import export_content as export_content_orig
+
+    class ExportDexterity(grok.View):
+        """Returns a list of paths of all items found by the catalog.
+           This view return all elements from context called
+        """
+        grok.context(Interface)
+        grok.name('export_dexterity')
+        grok.require('cmf.ManagePortal')
+
+        def render(self):
+            if 'dir' not in self.request.form:
+                raise ValueError("Mandatory parameter 'dir' was not specified")
+            directory = self.request.form['dir']
+            return export_content_orig(
+                self.context,
+                basedir=directory,  # export directory
+                extra_skip_classname=['ATTopic']
+            )
+except:
+    pass
