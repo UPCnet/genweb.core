8223b39076679a1265244aeed12f766214ba2184
Corina Riba <corina.riba@upcnet.es>
2018-07-17 13:58:06 +0200

    Back to development: 4.8.54

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.8.53'
+version = '4.8.54.dev0'
 
 README = open('README.rst').read()
 HISTORY = open(os.path.join('docs', 'HISTORY.rst')).read()
