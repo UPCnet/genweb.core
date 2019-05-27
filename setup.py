c853a97908331ab37a777f2533bcc221fd8eddc4
Iago López Fernández <iago.lopez@upcnet.es>
2019-05-27 11:56:54 +0200

    Back to development: 4.8.62

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.8.61'
+version = '4.8.62.dev0'
 
 README = open('README.rst').read()
 HISTORY = open(os.path.join('docs', 'HISTORY.rst')).read()
