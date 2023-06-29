d4b34409e35df2354b113f662ef7f102b86833bb
Pilar Marinas <pilar.marinas@upcnet.es>
2023-06-29 10:02:27 +0200

    Fix migrate ploneformgen to easyform

--- genweb/core/patches.py
+++ genweb/core/patches.py
@@ -985,3 +985,101 @@ def sitemapObjects(self):
             #'changefreq': 'always', # hourly/daily/weekly/monthly/yearly/never
             #'prioriy': 0.5, # 0.0 to 1.0
         }
+
+LOG = logging.getLogger('ATCT.migration')
+
+def walk(self):
+    """Walks around and returns all objects which needs migration
+    :return: objects (with acquisition wrapper) that needs migration
+    :rtype: generator
+    """
+    catalog = self.catalog
+    query = {
+        'portal_type' : self.src_portal_type,
+        'meta_type' : self.src_meta_type,
+        'path' : "/".join(self.portal.getPhysicalPath()),
+    }
+    # Comentado para que funcione la migracion de PloneFormGen a EasyForm
+    # if HAS_LINGUA_PLONE and 'Language' in catalog.indexes():
+    #     query['Language'] = 'all'
+
+    brains = catalog(query)
+    limit = getattr(self, 'limit', False)
+    if limit:
+        brains = brains[:limit]
+    for brain in brains:
+        try:
+            obj = brain.getObject()
+            #Añadir que si tiene layout le ponga el view porque pfg_draft_view ya no funciona
+            if obj.getLayout() != None:
+                obj.setLayout('view')
+        except AttributeError:
+            LOG.error("Couldn't access %s" % brain.getPath())
+            continue
+        try:
+            state = obj._p_changed
+        except:
+            state = 0
+        if obj is not None:
+            yield obj
+            # safe my butt
+            if state is None:
+                obj._p_deactivate()
+
+
+from ast import literal_eval
+from collective.easyform.api import get_actions
+from collective.easyform.api import get_fields
+from collective.easyform.interfaces import ISaveData
+from DateTime import DateTime
+from plone.namedfile.interfaces import INamedBlobFileField
+from zope.schema.interfaces import IDate
+from zope.schema.interfaces import IDatetime
+from zope.schema.interfaces import IFromUnicode
+from zope.schema.interfaces import ISet
+
+import logging
+logger = logging.getLogger('collective.easyform.migration')
+
+
+def migrate_saved_data(ploneformgen, easyform):
+    for data_adapter in ploneformgen.objectValues('FormSaveDataAdapter'):
+        actions = get_actions(easyform)
+        action = actions.get(data_adapter.getId())
+        schema = get_fields(easyform)
+        if ISaveData.providedBy(action):
+            cols = data_adapter.getColumnNames()
+            for idx, row in enumerate(data_adapter.getSavedFormInput()):
+                if len(row) != len(cols):
+                    logger.warning(
+                        'Number of columns does not match. Skipping row %s in '
+                        'data adapter %s/%s', idx,
+                        '/'.join(easyform.getPhysicalPath()),
+                        data_adapter.getId())
+                    continue
+                data = {}
+                for key, value in zip(cols, row):
+                    field = schema.get(key)
+                    value = value.decode('utf8')
+                    if IFromUnicode.providedBy(field):
+                        # Añadido try - except para que funcione
+                        try:
+                            value = field.fromUnicode(value)
+                        except:
+                            value = value
+                            pass
+                    elif IDatetime.providedBy(field) and value:
+                        value = DateTime(value).asdatetime()
+                    elif IDate.providedBy(field) and value:
+                        value = DateTime(value).asdatetime().date()
+                    elif ISet.providedBy(field):
+                        # Añadido try - except para que funcione
+                        try:
+                            value = set(literal_eval(value))
+                        except:
+                            value = set(value)
+                            pass
+                    elif INamedBlobFileField.providedBy(field):
+                        value = None
+                    data[key] = value
+                action.addDataRow(data)
